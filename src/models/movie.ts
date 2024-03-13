816a3fa1a86e09212199c24283f6996d61fb3066
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-03-13 15:32:06 +0100

    movie middleware

--- src/models/movie.ts
+++ src/models/movie.ts
@@ -14,4 +14,29 @@ export interface Movie {
     views: number;
     genres?: MovieGenre[];
     userWhoBookmarkedIt?: Favorite[];
-  }
+}
+
+export interface MoviePatch {
+    id?: number;
+    title?: string;
+    videoSrc?: string;
+    photoSrc?: string;
+    trailerSrc?: string;
+    duration?: string;
+    ratingImdb?: number;
+    releaseYear?: number;
+    description?: string;
+    views?: number;
+}
+
+export interface MoviePost {
+    title: string;
+    videoSrc: string;
+    photoSrc: string;
+    trailerSrc: string;
+    duration: string;
+    ratingImdb: number;
+    releaseYear: number;
+    description: string;
+    views: number;
+}
