1ada4e231aacd32e41e0c67e9d261ed002784421
Jurgen Hasmeta <j.hasmeta.ext@teamsystem.com>
2024-03-14 22:43:01 +0100

    added stuff

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -253,6 +253,403 @@ export const options = {
                 },
             },
         },
+        tags: [
+            {
+                name: 'Movies',
+                description: 'The movies managing API',
+            },
+            {
+                name: 'Authentication',
+                description: 'The authentication managing API',
+            },
+        ],
+        paths: {
+            '/movies': {
+                get: {
+                    summary: 'Returns the list of all the movies',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    responses: {
+                        '200': {
+                            description: 'The list of the movies',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'array',
+                                        items: {
+                                            $ref: '#/components/schemas/Movie',
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+                post: {
+                    summary: 'Create a new movie',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/MoviePost',
+                                },
+                            },
+                        },
+                    },
+                    responses: {
+                        '200': {
+                            description: 'The movie was successfully created',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/schemas/MoviePost',
+                                    },
+                                },
+                            },
+                        },
+                        '500': {
+                            description: 'Some server error',
+                        },
+                    },
+                },
+            },
+            '/movies/{id}': {
+                get: {
+                    summary: 'Get the movie by id',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    parameters: [
+                        {
+                            in: 'path',
+                            name: 'id',
+                            schema: {
+                                type: 'number',
+                            },
+                            required: true,
+                            description: 'The movie id',
+                        },
+                    ],
+                    responses: {
+                        '200': {
+                            description: 'The movie description by id',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/schemas/Movie',
+                                    },
+                                },
+                            },
+                        },
+                        '404': {
+                            description: 'The movie was not found',
+                        },
+                    },
+                },
+                delete: {
+                    summary: 'Remove the movie by id',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    parameters: [
+                        {
+                            in: 'path',
+                            name: 'id',
+                            schema: {
+                                type: 'number',
+                            },
+                            required: true,
+                            description: 'The movie id',
+                        },
+                    ],
+                    responses: {
+                        '200': {
+                            description: 'The movie was deleted',
+                        },
+                        '404': {
+                            description: 'The movie was not found',
+                        },
+                    },
+                },
+                patch: {
+                    summary: 'Update the movie by the id',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    parameters: [
+                        {
+                            in: 'path',
+                            name: 'id',
+                            schema: {
+                                type: 'number',
+                            },
+                            required: true,
+                            description: 'The movie id to update',
+                        },
+                    ],
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/MoviePatch',
+                                },
+                            },
+                        },
+                    },
+                    responses: {
+                        '200': {
+                            description: 'The movie was updated',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/schemas/MoviePatch',
+                                    },
+                                },
+                            },
+                        },
+                        '404': {
+                            description: 'The movie was not found',
+                        },
+                        '500': {
+                            description: 'Some error happened',
+                        },
+                    },
+                },
+                put: {
+                    summary: 'Update the movie by the id',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    parameters: [
+                        {
+                            in: 'path',
+                            name: 'id',
+                            schema: {
+                                type: 'number',
+                            },
+                            required: true,
+                            description: 'The movie id to update',
+                        },
+                    ],
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    $ref: '#/components/schemas/MoviePost',
+                                },
+                            },
+                        },
+                    },
+                    responses: {
+                        '200': {
+                            description: 'The movie was updated',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/schemas/MoviePost',
+                                    },
+                                },
+                            },
+                        },
+                        '404': {
+                            description: 'The movie was not found',
+                        },
+                        '500': {
+                            description: 'Some error happened',
+                        },
+                    },
+                },
+            },
+            '/movies/{title}': {
+                get: {
+                    summary: 'Get the movie by title',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    parameters: [
+                        {
+                            in: 'path',
+                            name: 'title',
+                            schema: {
+                                type: 'string',
+                            },
+                            required: true,
+                            description: 'The movie title',
+                        },
+                    ],
+                    responses: {
+                        '200': {
+                            description: 'The movie description by title',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        $ref: '#/components/schemas/Movie',
+                                    },
+                                },
+                            },
+                        },
+                        '404': {
+                            description: 'The movie was not found',
+                        },
+                    },
+                },
+            },
+            '/latestMovies': {
+                get: {
+                    summary: 'Returns the list of the latest movies',
+                    tags: ['Movies'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    responses: {
+                        '200': {
+                            description: 'The list of the latest movies',
+                            content: {
+                                'application/json': {
+                                    schema: {
+                                        type: 'array',
+                                        items: {
+                                            $ref: '#/components/schemas/Movie',
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+            '/register': {
+                post: {
+                    summary: 'Register a new user',
+                    tags: ['Authentication'],
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    type: 'object',
+                                    properties: {
+                                        email: {
+                                            type: 'string',
+                                            format: 'email',
+                                            description: 'Email address of the user',
+                                        },
+                                        password: {
+                                            type: 'string',
+                                            minLength: 6,
+                                            pattern: '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W_]).{6,}$',
+                                            description: 'Password of the user',
+                                        },
+                                        userName: {
+                                            type: 'string',
+                                            minLength: 3,
+                                            pattern: '^[a-zA-Z0-9]*$',
+                                            description: 'Username of the user',
+                                        },
+                                    },
+                                    required: ['email', 'password', 'userName'],
+                                },
+                            },
+                        },
+                    },
+                    responses: {
+                        200: {
+                            description: 'User registered successfully',
+                        },
+                        400: {
+                            description: 'Invalid request data',
+                        },
+                    },
+                },
+            },
+            '/login': {
+                post: {
+                    summary: 'Login to the application',
+                    tags: ['Authentication'],
+                    requestBody: {
+                        required: true,
+                        content: {
+                            'application/json': {
+                                schema: {
+                                    type: 'object',
+                                    properties: {
+                                        email: {
+                                            type: 'string',
+                                            format: 'email',
+                                            description: 'Email address of the user',
+                                        },
+                                        password: {
+                                            type: 'string',
+                                            minLength: 6,
+                                            pattern: '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W_]).{6,}$',
+                                            description: 'Password of the user',
+                                        },
+                                    },
+                                    required: ['email', 'password'],
+                                },
+                            },
+                        },
+                    },
+                    responses: {
+                        200: {
+                            description: 'User logged in successfully',
+                        },
+                        400: {
+                            description: 'Invalid request data',
+                        },
+                    },
+                },
+            },
+            '/validateUser': {
+                get: {
+                    summary: 'Validate User',
+                    tags: ['Authentication'],
+                    security: [
+                        {
+                            bearerAuth: [],
+                        },
+                    ],
+                    responses: {
+                        200: {
+                            description: 'User is authenticated',
+                        },
+                        401: {
+                            description: 'Unauthorized - Token is missing or invalid',
+                        },
+                    },
+                },
+            },
+        },
     },
     security: [
         {
